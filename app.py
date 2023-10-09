038a7f2c6f896238fb284b7dfbe98631bd324926
namraha19 <namraha19@gmail.com>
2023-10-09 21:21:33 +0530

    PROJECT COMPLETED

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
 import streamlit as st
+from functions import *
 st.title("YouTube comment scrapper")
 link=st.text_input("paste youtube link here:")
-st.button("Fetch")
-st.table()
+data=[]
+def fetch_data(link):
+    get_data(link,data)
+st.button("Fetch",on_click=fetch_data(link))
+st.table(data)
+# st.dataframe(data,use_container_width=True)
---
+++ functions.py
@@ -0,0 +1,37 @@
+import requests
+from googleapiclient.discovery import build
+api_key="AIzaSyDxQkZ1ik7V9uMZ4QaH3GQFcnIlySU2BBI"
+youtube=build("youtube",'v3',developerKey=api_key)
+
+
+def get_video_id(link:str)->str:
+    i=link.find("watch?v=")
+    link=link[i+8:]
+    return link
+
+def get_page_data(videoid:str,next_page_token=None):
+    request=youtube.commentThreads().list(part='snippet',videoId=videoid,maxResults=100,textFormat='plainText',pageToken=next_page_token)
+    response=request.execute()
+    return response
+
+
+def fetch_comments(response,data):
+    for item in response.get('items'):
+        obj=item.get('snippet').get('topLevelComment')
+        name=obj.get('snippet').get('authorDisplayName')
+        comment=obj.get('snippet').get('textDisplay')
+        data.append((name,comment))
+    next_page_token=response.get('nextPageToken')
+    return (next_page_token)
+
+def get_data(link:str,data:list,pageToken=None)->list:
+    try:
+        videoid=get_video_id(link)
+        response=get_page_data(videoid,pageToken)
+        next_page_token=fetch_comments(response,data)
+        if(len(data)>1000):
+            return
+        else:
+            get_data(link,data,next_page_token)
+    except Exception as e:
+        return
